422cede33cece9f2b29e935fb8015bdd2f26cb69
Abdirahman04 <abdixcom@gmail.com>
2024-01-03 12:59:02 +0300

    added withdraw functionality

--- src/views/transactions/Withdraw.vue
+++ src/views/transactions/Withdraw.vue
@@ -10,14 +10,14 @@
             <div class="row">
                 <form class="p-3 border border-3 border-primary">
                     <div class="row form-floating">
-                        <input type="number" id="amount" class="form-control" placeholder="amount">
+                        <input type="number" id="amount" class="form-control" placeholder="amount" v-model="fetchBody.balance">
                         <label for="amount" class="form-label">Enter amount</label>
                     </div>
                     <div class="row form-floating">
-                        <input type="password" id="pass" class="form-control" placeholder="password">
+                        <input type="password" id="pass" class="form-control" placeholder="password" v-model="form.password">
                         <label for="pass" class="form-label">Enter password</label>
                     </div>
-                    <button id="btn" class="btn btn-info">Withdraw</button>
+                    <button id="btn" class="btn btn-info" @click.prevent="withdrawAmount(form,fetchBody)">Withdraw</button>
                 </form>
             </div>
             <div class="row"><div class="col"><h3 id="err"></h3></div></div>
@@ -26,11 +26,41 @@
 </template>
 
 <script>
+import { login,withdraw,getUserByAccNumberRaw } from '@/utils/apiService.js'
+
 export default {
+    data() {
+        return {
+            form: {
+                accountNumber: JSON.parse(localStorage.getItem('accountNumber')),
+                password: ''
+            },
+            fetchBody: {
+                id: JSON.parse(localStorage.getItem('userData')).id,
+                balance: ''
+            }
+        };
+    },
     methods: {
         back() {
             this.$router.push({ name: 'dashboard' })
-        }
+        },
+        withdrawAmount(loginBody, withdrawBody) {
+            login(loginBody).then(data => {
+                if(data) {
+                    withdraw(withdrawBody).then(res => {
+                        if (!res.ok) {
+                            throw new Error(`HTTP error! Status: ${res.status}`);
+                        }
+                        getUserByAccNumberRaw(this.form.accountNumber).then(data => {
+                            localStorage.setItem('userData', JSON.stringify(data));
+                            this.$router.push({ name: 'dashboard' });
+                        })
+                    })
+                }
+                else alert('Incorrect password!');
+            })
+        },
     }
 }
 </script>
