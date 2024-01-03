47260c0a8ff1ea0c527e5cc63cdbea0783f86d7d
Abdirahman04 <abdixcom@gmail.com>
2024-01-03 16:07:36 +0300

    added transfer functionality

--- src/views/transactions/Transfer.vue
+++ src/views/transactions/Transfer.vue
@@ -10,18 +10,18 @@
             <div class="row">
                 <form class="p-3 border border-3 border-primary">
                     <div class="row form-floating">
-                        <input type="number" id="amount" class="form-control" placeholder="amount">
+                        <input type="number" id="amount" class="form-control" placeholder="amount" v-model="fetchBody.balance">
                         <label for="amount" class="form-label">Enter amount</label>
                     </div>
                     <div class="row form-floating">
-                        <input type="number" id="recipient" class="form-control" placeholder="recipient">
+                        <input type="number" id="recipient" class="form-control" placeholder="recipient" v-model="fetchBody.recipientId">
                         <label for="recipient" class="form-label">Who do you want to transfer to</label>
                     </div>
                     <div class="row form-floating">
-                        <input type="password" id="pass" class="form-control" placeholder="password">
+                        <input type="password" id="pass" class="form-control" placeholder="password" v-model="form.password">
                         <label for="pass" class="form-label">Enter password</label>
                     </div>
-                    <button id="btn" class="btn btn-info">Transfer</button>
+                    <button id="btn" class="btn btn-info" @click.prevent="transferAmount(form,fetchBody)">Transfer</button>
                 </form>
             </div>
             <div class="row"><div class="col"><h3 id="err"></h3></div></div>
@@ -30,11 +30,52 @@
 </template>
 
 <script>
+import { send,login,getUserByAccNumberRaw } from '@/utils/apiService.js'
+
 export default {
+    data() {
+        return {
+            form: {
+                accountNumber: JSON.parse(localStorage.getItem('accountNumber')),
+                password: ''
+            },
+            fetchBody: {
+                senderId: JSON.parse(localStorage.getItem('userData')).id,
+                recipientId: '',
+                balance: ''
+            }
+        };
+    },
     methods: {
         back() {
             this.$router.push({ name: 'dashboard' })
-        }
+        },
+        transferAmount(loginBody, transferBody) {
+            console.log(transferBody);
+            login(loginBody).then(data => {
+                if(data) {
+                    getUserByAccNumberRaw(this.fetchBody.recipientId).then(data => {
+                        const sendBody = {
+                            senderId: this.fetchBody.senderId,
+                            recipientId: data.id,
+                            balance: this.fetchBody.balance
+                        }
+
+                        send(sendBody).then(res => {
+                            console.log(res);
+                            if (!res.ok) {
+                                throw new Error(`HTTP error! Status: ${res.status}`);
+                            }
+                            getUserByAccNumberRaw(this.form.accountNumber).then(data => {
+                                localStorage.setItem('userData', JSON.stringify(data));
+                                this.$router.push({ name: 'dashboard' });
+                            })
+                        })
+                    }).catch(err => alert(err))
+                }
+                else alert('Incorrect password!');
+            })
+        },
     }
 }
 </script>
