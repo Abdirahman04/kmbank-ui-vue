2a9707c2ce4a9a04212590659f5e8bc0a58e9c0a
Abdirahman04 <abdixcom@gmail.com>
2024-01-05 20:25:35 +0300

    added card component

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title"><h3>{{ title }}</h3></div>
+            <div class="card-text"><p>{{ text }}</p></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about container">
+  <div class="statistics container">
     <h1>Statistics Page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-8">
+          <div class="row">
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card-title"><h3>NO. of Users</h3></div>
+                  <div class="card-text">346</div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+            
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+            
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
